8a229f4e3ea2486ce512a4261a8a62fbad25f38a
bhayek12@gmail.com <Ben@Bens-MacBook-Pro-2.local>
2018-09-10 12:07:11 -0600

    Input field takes text and console logs it

--- weathrly/src/Header.js
+++ weathrly/src/Header.js
@@ -5,9 +5,20 @@ export default class Header extends Component {
   constructor(props) {
     super(props)
     this.state= {
-      inputValue: '',
-    }
+      value: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
 }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    console.log('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
 
   // updateInputValue = (e) => {
   //       this.setState({
@@ -27,16 +38,14 @@ render() {
       <h1 className='title'>Weathrly
       <img src={"/public/thermometer.svg"} />
       </h1>
-      <form className='search-form'>
-        <input className='search-bar 
+      <form className='search-form' onSubmit={this.handleSubmit}>
+        <input className='search-bar' 
           placeholder="Show me the weather in... city, zip, or place" 
          
-          value={this.state.inputValue} onChange={this.updateInputValue}' />
+          type="text" value={this.state.value} onChange={this.handleChange} />
           <button className='search-button'>Find Me</button>
       </form>
       </div>
     )
-
   }
-   
 }
