6e16fba8f0a13691e7f4ed0e97a14c7568464987
bhayek12@gmail.com <Ben@Bens-MacBook-Pro-2.local>
2018-09-11 11:47:15 -0600

    Fix bug with double click

--- weathrly/src/App.js
+++ weathrly/src/App.js
@@ -7,8 +7,6 @@ import CurrentWeatherCard from './CurrentWeatherCard';
 import TenDayCard from './TenDayCard';
 import apiConfig from './APIkey.js'
 
-// const weatherInfo = `http://api.wunderground.com/api/${apiConfig.apiKey}/conditions/hourly/forecast10day/q/${state}/${city}.json`
-
 class App extends Component {
   constructor(props) {
     super(props)
@@ -21,63 +19,43 @@ class App extends Component {
 }
 
 componentDidMount = () => {
-  // this.getFromLocalStorage()
   let storedLocation = JSON.parse(localStorage.getItem('weathrly', this.state.location))
   let autoLocation = 'autoip'
     if(storedLocation){
-    this.getLocation(this.state.location = storedLocation)
+    this.getLocation(storedLocation)
   } else {
-    this.fetchApi(this.state.location = autoLocation)
+    this.fetchApi(autoLocation)
     console.log(storedLocation)
   }
-}
-
-componentWillMount(){
-  let storedLocation = JSON.parse(localStorage.getItem('weathrly', this.state.location))
-    if(localStorage.getItem('weathrly', this.state.location)){
-    this.fetchApi('weathrly', this.state.location)
-  }
-  console.log('weathrly', this.state.location)
 }
 
 updateLocalStorage = () => {
     localStorage.setItem('weathrly', JSON.stringify(this.state.location))
   }
 
-  getFromLocalStorage = () => {
-    const currentLocation = localStorage.getItem(this.state.location)
-
-    if (currentLocation) {
-      this.setState({ currentLocation: JSON.parse(currentLocation) })
-    }
-    console.log(currentLocation)
-  }
-
 fetchApi(location) {
-  fetch(`http://api.wunderground.com/api/${apiConfig.apiKey}/conditions/hourly/forecast10day/q/${this.state.location}.json`)
+  fetch(`http://api.wunderground.com/api/${apiConfig.apiKey}/conditions/hourly/forecast10day/q/${location}.json`)
     .then(data => data.json())
-    // console.log(data.json())
-    .then(data => {this.setState({
-     isLoaded: true,
-     weatherCards: data,
-     location: location
-    })})
+    .then(data => {
+      this.setState({
+        isLoaded: true,
+        weatherCards: data,
+        location: location
+      })
+    })
     .catch(error => {
       console.log(error)
     })
 }
 
   getLocation = (location) => {
-    this.setState({
-      location: location 
-    })
+
     this.fetchApi(location)
     this.updateLocalStorage()
   }
 
 
   render() {
-    // componentDidMount();
     let {isLoaded, weatherInfo} = this.state
       if(!isLoaded){
         return <div> Loading.... </div>
