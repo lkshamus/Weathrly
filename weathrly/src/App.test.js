ec908ff19cf1fdab426aeb0fb8f653f9c3539a7f
Laura Shamus <lkshamus@gmail.com>
2018-09-11 19:01:54 -0600

    Initial testing

--- weathrly/src/App.test.js
+++ weathrly/src/App.test.js
@@ -1,9 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow, mount } from 'enzyme';
 import App from './App';
+
+describe('App', () => {
+
+  let wrapper 
+
+  beforeEach(() => {
+    wrapper = shallow(<App />)
+  })
+
+it('should exist', () => {
+  expect(wrapper).toBeDefined();
+})
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+
+it('should retrieve data from local storage on mount', () =>{
+
+  const location = [
+    {'weathrly': 'Denver, CO'},
+  ]
+
+  localStorage.setItem('weathrly', JSON.stringify(location))
+
+  wrapper = mount(<App />);
+
+  expect(wrapper.state().location).toEqual({'weathrly': 'Denver, CO'});
+})
+})
