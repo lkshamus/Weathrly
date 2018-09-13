50168ac7ca308b8c3f6d0103e22080e69c5d58d0
bhayek12@gmail.com <Ben@Bens-MacBook-Pro-2.local>
2018-09-12 18:30:15 -0600

    Current weather test passing

--- weathrly/src/CurrentWeather.test.js
+++ weathrly/src/CurrentWeather.test.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import { shallow, mount } from 'enzyme';
 import CurrentWeatherCard from './CurrentWeatherCard';
+import {data} from './API';
 
 describe('CurrentWeatherCard', () => {
   
   let wrapper;
+  let weatherCards = data;
 
   beforeEach(()=> {
-    wrapper = shallow(<CurrentWeatherCard />)
+    wrapper = shallow(<CurrentWeatherCard weather={data}/>)
   })
 
   it('should exist', () => {
